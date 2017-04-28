d01d2884405f0bdf90439a94512fa0db8628d97d
Abraham Haskins <abeisgreat@abeisgreat.com>
2017-04-28 15:13:11 -0700

    Attemps to fix env issue

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,16 @@
+var functions = require('firebase-functions');
 var modules = [
-  require("./cool.js")
-  require("./purge.js"),
+  require("./cool.js"),
+  require("./purge.js")
 ]
+
+process.env.azure_key = functions.config().env.azure_key;
+process.env.gcs_bucket = functions.config().env.gcs_bucket;
 
 modules.forEach(function (mod) {
   Object.keys(mod).forEach(function (func) {
     exports[func] = mod[func];
   });
 });
+
+console.log(exports)
