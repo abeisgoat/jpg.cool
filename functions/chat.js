0991111a45c9d4a60c0797985a990a5f2c46d0c5
Abraham Haskins <abeisgreat@abeisgreat.com>
2017-07-17 13:18:43 -0700

    Adds reporting chat message

--- functions/chat.js
+++ functions/chat.js
@@ -38,6 +38,42 @@ exports.notify =
       });
   });
 
+exports.notify_reported =
+  functions.database.ref('/cache/{task}/reported')
+  .onWrite((event) => {
+    return event.data.ref.parent.once("value")
+      .then((ss) => {
+        return ss.val();
+      })
+      .then((entry) => {
+        if (!entry.reported) return;
+
+        const params = {
+          key: functions.config().hangouts.key,
+          token: functions.config().hangouts.token
+        };
+
+        const message = {};
+
+        if (!entry.error) {
+          message.text = `@all this link was reported by a user https://jpg.cool/${entry.phrase}`;
+        }
+
+        const body = JSON.stringify(message);
+        const url = `https://dynamite.sandbox.googleapis.com/v1/rooms/AAAAiJBrU5o/webhooks?key=${params.key}&token=${params.token}`;
+        console.log(url);
+        console.log(body);
+
+        return fetch(url, {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          method: "POST",
+          body
+        });
+      });
+  });
+
 exports.incoming_notify =
   functions.https.onRequest((req, res) => {
     console.log("Got a message");
